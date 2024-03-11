3ba420e97b19b6675054e1c361560c7d742ac748
Artyome <flex785834@rambler.ru>
2024-03-11 19:30:01 +0300

    HW1 SWIFTAppsGB ready

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Артем Сиренко on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gb/gb/gbApp.swift
@@ -0,0 +1,17 @@
+//
+//  gbApp.swift
+//  gb
+//
+//  Created by Артем Сиренко on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct gbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
